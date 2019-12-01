b9a83175c9e0a853690c53c9ca974de621eded13
trémeur <eheu48@gmail.com>
2019-12-01 13:57:46 +0000

    Add zsh aliases 🐝

--- scripts/dotset.sh
+++ scripts/dotset.sh
@@ -2,7 +2,10 @@
 
 mv ~/mdddots ~/.linux
 
+# Before doing this cmus and oh-my-zsh should be installed
+
 ln -s ~/.linux/dots/.zshrc ~
+ln -s ~/.linux/dots/aliases ~/.oh-my-zsh/custom
 ln -s ~/.linux/dots/.gitconfig ~
 ln -s ~/.linux/dots/.XCompose ~
 ln -s ~/.linux/dots/.conkyrc ~
