37dd456d38f941cb3e59049c892cde8521287110
trémeur <eheu48@gmail.com>
2019-12-01 22:43:12 +0000

    You have to delete them first

--- scripts/dotset.sh
+++ scripts/dotset.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+### Before doing this oh-my-zsh and cmus should be installed
+
+# Rename the git repo so scripts point right
 mv ~/mdddots ~/.linux
 
-# Before doing this cmus and oh-my-zsh should be installed
+# Get rid of existing files
+rm ~/.zshrc ~/.gitconfig ~./XCompose ~/.conkyrc
+rm ~/.config/cmus/rc
+rm ~/.oh-my-zsh/custom/aliases
 
+# Symlink files from git repo
 ln -s ~/.linux/dots/.zshrc ~
 ln -s ~/.linux/dots/aliases ~/.oh-my-zsh/custom
 ln -s ~/.linux/dots/.gitconfig ~
