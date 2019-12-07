eb5ac23bce2f9af631633d5f016a59485b42169b
trémeur <eheu48@gmail.com>
2019-12-07 16:24:53 +0000

    Allow for nonexistence of files

--- scripts/dotset.sh
+++ scripts/dotset.sh
@@ -6,15 +6,16 @@
 mv ~/mdddots ~/.linux
 
 # Get rid of existing files
-rm ~/.zshrc ~/.gitconfig ~/.XCompose
-rm ~/.config/cmus/rc
-rm ~/.oh-my-zsh/custom/aliases
+rm -f ~/.zshrc ~/.gitconfig ~/.XCompose
+rm -f ~/.config/cmus/rc
+rm -f ~/.oh-my-zsh/custom/aliases
 
 # Symlink files from git repo
 ln -s ~/.linux/dots/.zshrc ~
 ln -s ~/.linux/dots/aliases ~/.oh-my-zsh/custom
 ln -s ~/.linux/dots/.gitconfig ~
 ln -s ~/.linux/dots/.XCompose ~
+mkdir =p ~/.config/cmus
 ln -s ~/.linux/dots/cmus/rc ~/.config/cmus
 
 ## Prepare for colorus
