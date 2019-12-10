588234d467811d66d59a2551e750fdcc2b1f6ef4
trémeur <eheu48@gmail.com>
2019-12-10 17:29:26 +0000

    Add plank + gtk prefs

--- scripts/dotset.sh
+++ scripts/dotset.sh
@@ -11,6 +11,9 @@ rm -f ~/.config/cmus/rc
 rm -f ~/.oh-my-zsh/custom/aliases
 rm -f ~/.config/openbox/*
 rm -f ~/.config/rofi/config.rasi
+rm -f ~/.config/gtk-3.0/settings.ini
+rm -f ~/.config/gtk-3.0/gtk.css
+rm -f ~/.config/gtk-4.0/settings.ini
 
 # Symlink files from git repo
 ln -s ~/.linux/dots/.zshrc ~
@@ -25,6 +28,11 @@ ln -s ~/.linux/dots/openbox/rc.xml ~/.config/openbox
 ln -s ~/.linux/dots/openbox/menu.xml ~/.config/openbox
 mkdir -p ~/.config/rofi
 ln -s ~/.linux/dots/rofi/config.rasi ~/.config/rofi
+mkdir -p ~/.config/gtk-3.0
+ln -s ~/.linux/dots/gtk/3/settings.ini ~/.config/gtk-3.0
+ln -s ~/.linux/dots/gtk/3/gtk.css ~/.config/gtk-3.0
+mkdir -p ~/.config/gtk-4.0
+ln -s ~/.linux/dots/gtk/4/settings.ini ~/.config/gtk-4.0
 
 ## Prepare for colorus
 touch ~/.colourvars
