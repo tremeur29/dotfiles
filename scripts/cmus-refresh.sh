cbff1bb2362ccc8532f8ef02aa9189134cbe6853
trémeur <eheu48@gmail.com>
2020-02-05 00:12:16 +0000

    Update cmus script

--- scripts/cmus-refresh.sh
+++ scripts/cmus-refresh.sh
@@ -2,6 +2,10 @@
 
 # see https://github.com/cmus/cmus/issues/233
 
-cmus-remote -C clear
-cmus-remote -C "add ~/Music"
-cmus-remote -C "update-cache -f"
+if cmus-remote -Q &>/dev/null ; then
+   cmus-remote -C clear
+   cmus-remote -C "add ~/Music"
+   cmus-remote -C "update-cache -f"
+else
+   echo Start cmus first. 
+fi
