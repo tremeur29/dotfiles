a795f1a6352433a61cc9f4cb1561048197932a2c
trémeur <eheu48@gmail.com>
2020-05-20 18:19:45 +0100

    Move emacs files to separate repository

--- scripts/today.sh
+++ scripts/today.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-emacs -batch -Q -l ~/.linux/dots/emacs/agenda.el -eval '(org-batch-agenda "a" org-agenda-span 1)' 
+emacs -batch -Q -l ~/.emacs.d/agenda.el -eval '(org-batch-agenda "a" org-agenda-span 1)' 
