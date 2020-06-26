cf497be66437fb2fdd2ad7b0dafef52f740471a2
trémeur <eheu48@gmail.com>
2020-06-26 21:47:31 +0100

    Fix album artist issue?

--- scripts/nowplaying.sh
+++ scripts/nowplaying.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
 
-ARTIST=$( cmus-remote -Q 2>/dev/null | grep "tag artist" | cut -d " " -f 3- )
+ARTIST=$( cmus-remote -Q 2>/dev/null | grep "tag artist " | cut -d " " -f 3- )
 TITLE=$( cmus-remote -Q 2>/dev/null | grep "tag title" | cut -d " " -f 3- )
 SPOTIFY_PID="$(pidof -s spotify || pidof -s .spotify-wrapped)"
 
